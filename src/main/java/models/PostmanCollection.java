e0bcd929add10732480ecaec01544ad3a98008e8
Jonathan Ling <jonathanling@shamrocksweb.com>
2023-01-13 10:40:05 -0500

    Initial commit.

---
+++ src/main/java/models/PostmanCollection.java
@@ -0,0 +1,16 @@
+package models;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class PostmanCollection {
+    private PostmanInformation info;
+    private List<PostmanItem> item;
+    private List<PostmanEvent> event;
+    private List<PostmanVariable> variable;
+    private PostmanAuth auth;
+    private Map<String, Object> protocolProfileBehavior;
+}
